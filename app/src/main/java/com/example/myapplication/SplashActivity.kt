adaddb4a34b30b4aaafc623fa8f82ba3365ec1e5
Abdelrahman Tamer <abdelrahmantamer0111@gmail.com>
2025-07-07 14:35:46 +0300

    make dashboard card  second part

--- app/src/main/java/com/example/myapplication/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -1,17 +1,28 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var get_start_button:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
     }
 
+    override fun onResume() {
+        super.onResume()
+        get_start_button=findViewById(R.id.start_bt)
+        get_start_button.setOnClickListener {
+            var intent=Intent(this,DashboardActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
 }
